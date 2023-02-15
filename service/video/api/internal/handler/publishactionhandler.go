8072073111e3d7cbab2e5dc9a199debbc9c1b844
jordanpep <47922108+jordanpep@users.noreply.github.com>
2023-02-16 00:07:02 +0800

    add video part

---
+++ service/video/api/internal/handler/publishactionhandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"mini-douyin/service/video/api/internal/logic"
+	"mini-douyin/service/video/api/internal/svc"
+	"mini-douyin/service/video/api/internal/types"
+)
+
+func PublishActionHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Duyin_publish_action_request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewPublishActionLogic(r.Context(), svcCtx)
+		resp, err := l.PublishAction(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
