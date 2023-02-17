5beb52a98a7a863a7e17b84212e94da3eef57bb2
jordanpep <47922108+jordanpep@users.noreply.github.com>
2023-02-17 10:44:49 +0800

    video

--- service/video/api/internal/svc/servicecontext.go
+++ service/video/api/internal/svc/servicecontext.go
@@ -1,15 +1,20 @@
 package svc
 
 import (
+	"github.com/zeromicro/go-zero/zrpc"
 	"mini-douyin/service/video/api/internal/config"
+	"mini-douyin/service/video/rpc/videoservice"
 )
 
 type ServiceContext struct {
 	Config config.Config
+
+	VideoRpc videoservice.Video
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:   c,
+		VideoRpc: videoservice.NewVideoService(zrpc.MustNewClient(c.VideoRpc)),
 	}
 }
